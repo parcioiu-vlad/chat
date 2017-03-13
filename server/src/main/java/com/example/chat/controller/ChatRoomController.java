614e5e81b46035c2c5c2c8733d482723dd0ed77b
vlad pircioiu <vlad pircioiu>
2017-03-13 12:39:03 +0200

    added spring boot socket example

---
+++ server/src/main/java/com/example/chat/controller/ChatRoomController.java
@@ -0,0 +1,35 @@
+package com.example.chat.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by vlad on 3/13/2017.
+ */
+@RestController
+@RequestMapping(value = "rest/v1/rooms")
+public class ChatRoomController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ChatRoomController.class);
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "OK";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "{roomId}")
+    public void postMessage(@RequestBody String message, @PathVariable Long roomId) {
+
+        LOG.debug("ChatRoomController -> postMessage - posting message " + message + " to room " + roomId);
+
+        simpMessagingTemplate.convertAndSend("/socket", message);
+    }
+}
